7941025c8ae691f409939e8879530239ed81eb51
SimonRiley-dev <santossiqueiragabriel98@gmail.com>
2026-04-28 10:12:06 -0300

    feita a adição de printf e case no código.

--- checkpoint2-dsa/src/main.c
+++ checkpoint2-dsa/src/main.c
@@ -1,8 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void calcularMedia();
+void calcularDerivada();
 
 int main() {
-    
+    int opcao;
+    do {
+        printf("\n--- SISTEMA ACADEMICO ---\n");
+        printf("1. Media e Classificacao\n");
+        printf("2. Derivada\n");
+        printf("3. Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
 
-    
+        switch(opcao) {
+            case 1: calcularMedia(); break;
+            case 2: calcularDerivada(); break;
+            case 3: printf("Saindo...\n"); break;
+            default: printf("Opcao invalida!\n");
+        }
+    } while(opcao != 3);
     return 0;
 }
