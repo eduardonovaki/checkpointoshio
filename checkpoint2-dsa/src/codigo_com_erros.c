da6f6619afcf81ff8eb3cf3483878c33510825ea
eduardonovaki <eduardo.novaki01@gmail.com>
2026-04-28 10:46:02 -0300

    Corrigi todos os erros do 'codigo com erros'

--- checkpoint2-dsa/src/codigo_com_erros.c
+++ checkpoint2-dsa/src/codigo_com_erros.c
@@ -7,9 +7,9 @@ int main() {
     scanf("%f", &nota1);
 
     printf("Digite a segunda nota: ");
-    scanf("%f", nota2);
+    scanf("%f", &nota2); //corrigindo bug colocando o "&"
 
-    media = nota1 + nota2 / 2;
+    media = (nota1 + nota2) / 2; //corrigido colocando parenteses para menter a lógica
 
     if (media >= 6) { // corrigido, trocando "=" por ">=" para manter a lógica
         printf("Aluno aprovado\n");
