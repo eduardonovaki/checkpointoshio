e7810d7d89a70146880bfb918437227cfeadf47c
eduardonovaki <eduardo.novaki01@gmail.com>
2026-04-28 10:30:27 -0300

    corrido parte dos erros do códido 'com erros'

--- checkpoint2-dsa/src/codigo_com_erros.c
+++ checkpoint2-dsa/src/codigo_com_erros.c
@@ -1,7 +1,7 @@
-#include "stdio.h"
+#include <stdio.h> //corrigido, trocando "" por <>
 
 int main() {
-    float nota1, nota2, media
+    float nota1, nota2, media; // corrigido, adicionando ";"
 
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
@@ -11,7 +11,7 @@ int main() {
 
     media = nota1 + nota2 / 2;
 
-    if (media = 6) {
+    if (media >= 6) { // corrigido, trocando "=" por ">=" para manter a lógica
         printf("Aluno aprovado\n");
     } else {
         printf("Aluno reprovado\n");
